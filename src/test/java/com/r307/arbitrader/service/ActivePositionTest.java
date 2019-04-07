100eb73d01c06908c66455ca7f5a170ca4825d0c
Peter Keeler <peter@bonevm.com>
2019-04-06 22:49:18 -0700

    JSON parser doesn't like Exchange due to circular references

---
+++ src/test/java/com/r307/arbitrader/service/ActivePositionTest.java
@@ -0,0 +1,68 @@
+package com.r307.arbitrader.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.r307.arbitrader.ExchangeBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+import static com.r307.arbitrader.DecimalConstants.USD_SCALE;
+import static org.junit.Assert.assertEquals;
+
+public class ActivePositionTest {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private CurrencyPair currencyPair = CurrencyPair.BTC_USD;
+    private BigDecimal exitTarget = new BigDecimal(0.003);
+    private Exchange longExchange = null;
+    private Exchange shortExchange = null;
+    private BigDecimal longVolume = new BigDecimal(1000.00);
+    private BigDecimal shortVolume = new BigDecimal(1000.00);
+    private BigDecimal longLimitPrice = new BigDecimal(5000.00);
+    private BigDecimal shortLimitPrice = new BigDecimal(5000.00);
+    private ActivePosition activePosition;
+
+    @Before
+    public void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+
+        longExchange = new ExchangeBuilder("Long", CurrencyPair.BTC_USD)
+            .withTradeService()
+            .withOrderBook(100, 100)
+            .withBalance(Currency.USD, new BigDecimal(100.00).setScale(USD_SCALE, RoundingMode.HALF_EVEN))
+            .build();
+        shortExchange = new ExchangeBuilder("Short", CurrencyPair.BTC_USD)
+            .withBalance(Currency.USD, new BigDecimal(500.00).setScale(USD_SCALE, RoundingMode.HALF_EVEN))
+            .build();
+    }
+
+    @Test
+    public void testSerialization() throws IOException{
+        activePosition = new ActivePosition();
+        activePosition.setCurrencyPair(currencyPair);
+        activePosition.setExitTarget(exitTarget);
+        activePosition.getLongTrade().setOrderId(UUID.randomUUID().toString());
+        activePosition.getLongTrade().setExchange(longExchange);
+        activePosition.getLongTrade().setVolume(longVolume);
+        activePosition.getLongTrade().setEntry(longLimitPrice);
+        activePosition.getShortTrade().setOrderId(UUID.randomUUID().toString());
+        activePosition.getShortTrade().setExchange(shortExchange);
+        activePosition.getShortTrade().setVolume(shortVolume);
+        activePosition.getShortTrade().setEntry(shortLimitPrice);
+
+        String json = OBJECT_MAPPER.writeValueAsString(activePosition);
+
+        ActivePosition deserialized = OBJECT_MAPPER.readValue(json.getBytes(Charset.forName("utf-8")), ActivePosition.class);
+
+        assertEquals(activePosition, deserialized);
+    }
+}
--- src/test/java/com/r307/arbitrader/service/TradingServiceTest.java
+++ src/test/java/com/r307/arbitrader/service/TradingServiceTest.java
@@ -1,41 +1,24 @@
 package com.r307.arbitrader.service;
 
+import com.r307.arbitrader.ExchangeBuilder;
 import com.r307.arbitrader.config.NotificationConfiguration;
 import com.r307.arbitrader.exception.OrderNotFoundException;
-import com.r307.arbitrader.config.ExchangeConfiguration;
 import com.r307.arbitrader.config.TradingConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 import org.knowm.xchange.Exchange;
-import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
-import org.knowm.xchange.dto.account.AccountInfo;
-import org.knowm.xchange.dto.account.Balance;
-import org.knowm.xchange.dto.account.Wallet;
-import org.knowm.xchange.dto.marketdata.OrderBook;
-import org.knowm.xchange.dto.trade.LimitOrder;
-import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
-import org.knowm.xchange.service.account.AccountService;
-import org.knowm.xchange.service.marketdata.MarketDataService;
-import org.knowm.xchange.service.trade.TradeService;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 import static com.r307.arbitrader.DecimalConstants.BTC_SCALE;
 import static com.r307.arbitrader.DecimalConstants.USD_SCALE;
-import static com.r307.arbitrader.service.TradingService.METADATA_KEY;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class TradingServiceTest {
@@ -45,7 +28,6 @@ public class TradingServiceTest {
     private Exchange shortExchange;
 
     private TradingConfiguration tradingConfiguration;
-    private NotificationConfiguration notificationConfiguration;
 
     private TradingService tradingService;
 
@@ -54,14 +36,14 @@ public class TradingServiceTest {
         MockitoAnnotations.initMocks(this);
 
         tradingConfiguration = new TradingConfiguration();
-        notificationConfiguration = new NotificationConfiguration();
+        NotificationConfiguration notificationConfiguration = new NotificationConfiguration();
 
-        longExchange = new ExchangeBuilder("Long")
+        longExchange = new ExchangeBuilder("Long", CurrencyPair.BTC_USD)
                 .withTradeService()
                 .withOrderBook(100, 100)
                 .withBalance(Currency.USD, new BigDecimal(100.00).setScale(USD_SCALE, RoundingMode.HALF_EVEN))
                 .build();
-        shortExchange = new ExchangeBuilder("Short")
+        shortExchange = new ExchangeBuilder("Short", CurrencyPair.BTC_USD)
                 .withBalance(Currency.USD, new BigDecimal(500.00).setScale(USD_SCALE, RoundingMode.HALF_EVEN))
                 .build();
 
@@ -192,116 +174,5 @@ public class TradingServiceTest {
 
         // the IOE should not propagate and blow everything up
         assertEquals(new BigDecimal(0.00).setScale(USD_SCALE, RoundingMode.HALF_EVEN), exposure);
-    }
-
-    private static class ExchangeBuilder {
-        private String name;
-        private Integer bids;
-        private Integer asks;
-        private List<Balance> balances = new ArrayList<>();
-        private TradeService tradeService = null;
-
-        ExchangeBuilder(String name) {
-            this.name = name;
-        }
-
-        ExchangeBuilder withOrderBook(int bids, int asks) {
-            this.bids = bids;
-            this.asks = asks;
-            return this;
-        }
-
-        ExchangeBuilder withBalance(Currency currency, BigDecimal amount) {
-            Balance balance = new Balance.Builder()
-                    .currency(currency)
-                    .available(amount)
-                    .build();
-
-            balances.add(balance);
-
-            return this;
-        }
-
-        ExchangeBuilder withTradeService() throws IOException {
-            tradeService = mock(TradeService.class);
-
-            LimitOrder order = new LimitOrder(
-                    Order.OrderType.BID,
-                    BigDecimal.TEN,
-                    currencyPair,
-                    UUID.randomUUID().toString(),
-                    new Date(),
-                    new BigDecimal(100.0000)
-                            .add(new BigDecimal(0.001))
-                            .setScale(BTC_SCALE, RoundingMode.HALF_EVEN));
-
-            when(tradeService.getOrder(eq("orderId"))).thenReturn(Collections.singleton(order));
-            when(tradeService.getOrder(eq("missingOrder"))).thenReturn(Collections.emptyList());
-            when(tradeService.getOrder(eq("notAvailable"))).thenThrow(new NotAvailableFromExchangeException("Exchange does not support fetching orders by ID"));
-            when(tradeService.getOrder(eq("ioe"))).thenThrow(new IOException("Unable to connect to exchange"));
-
-            return this;
-        }
-
-        Exchange build() throws IOException {
-            Exchange exchange = mock(Exchange.class);
-            ExchangeSpecification specification = mock(ExchangeSpecification.class);
-            ExchangeConfiguration metadata = new ExchangeConfiguration();
-
-            metadata.setHomeCurrency(Currency.USD);
-
-            when(exchange.getExchangeSpecification()).thenReturn(specification);
-            when(specification.getExchangeName()).thenReturn(name);
-            when(specification.getExchangeSpecificParametersItem(METADATA_KEY)).thenReturn(metadata);
-
-            if (bids != null || asks != null) {
-                MarketDataService marketDataService = mock(MarketDataService.class);
-                OrderBook orderBook = new OrderBook(
-                        new Date(),
-                        generateOrders(Order.OrderType.ASK),
-                        generateOrders(Order.OrderType.BID)
-                );
-
-                when(marketDataService.getOrderBook(eq(currencyPair))).thenReturn(orderBook);
-                when(exchange.getMarketDataService()).thenReturn(marketDataService);
-            }
-
-            if (!balances.isEmpty()) {
-                Wallet wallet = new Wallet(balances);
-                AccountInfo accountInfo = new AccountInfo(wallet);
-                AccountService accountService = mock(AccountService.class);
-
-                when(accountService.getAccountInfo()).thenReturn(accountInfo);
-                when(exchange.getAccountService()).thenReturn(accountService);
-            }
-
-            if (tradeService != null) {
-                when(exchange.getTradeService()).thenReturn(tradeService);
-            }
-
-            return exchange;
-        }
-
-        private static List<LimitOrder> generateOrders(Order.OrderType type) {
-            List<LimitOrder> orders = new ArrayList<>();
-
-            for (int i = 0; i < 100; i++) {
-                orders.add(new LimitOrder(
-                        type,
-                        BigDecimal.TEN,
-                        currencyPair,
-                        UUID.randomUUID().toString(),
-                        new Date(),
-                        new BigDecimal(100.0000)
-                                .add(new BigDecimal(i * 0.001))
-                                .setScale(BTC_SCALE, RoundingMode.HALF_EVEN)));
-            }
-
-            if (Order.OrderType.BID.equals(type)) {
-                Collections.reverse(orders);
-            }
-
-            return orders;
-        }
     }
 }
